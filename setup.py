630da5d3b895af16342bd0c83e0393c9c806d093
Young-Whee Lee <young.lee@hotmail.co.uk>
2017-10-07 16:59:00 +0100

    ~

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Fri Oct  6 23:58:45 2017
 from setuptools import setup, find_packages
 
 setup(name='model-building-lab',
-      version='0.4.1',
+      version='0.4.2',
       author='Young Lee',
       description='model-building-lab is a Python module for structuring, simplifying and accelerating machine learning projects',
       author_email='young-whee.lee@cantab.net',
